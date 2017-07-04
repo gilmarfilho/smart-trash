1f10b29974e0b80ccf51319b9b1a0880ed0653f8
Vinicius C. Lopes <vinilopes03@gmail.com>
2017-07-04 04:53:14 -0300

    New adds

--- app/src/main/java/com/st/smarttrash/MainActivity.java
+++ app/src/main/java/com/st/smarttrash/MainActivity.java
@@ -1,7 +1,8 @@
 package com.st.smarttrash;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new TrashFragment ())
+                    .add(R.id.container, new TrashFragment())
                     .commit();
         }
     }
