e53306f74dc845bf2f72ac84471a221d5847fc73
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2017-07-05 10:24:12 -0300

    Adicionado botão para insert na mainactivity

--- app/src/main/java/com/st/smarttrash/MainActivity.java
+++ app/src/main/java/com/st/smarttrash/MainActivity.java
@@ -1,7 +1,10 @@
 package com.st.smarttrash;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container, new TrashFragment())
                     .commit();
         }
+
+        Button btn = (Button) findViewById(R.id.insert_key);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, InsertKey.class));
+            }
+        });
+
     }
 }
