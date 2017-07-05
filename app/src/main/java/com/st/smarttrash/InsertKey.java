3bd87020a886216fc3c24ae2e6efe9150279db90
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2017-07-05 09:50:18 -0300

    Criada a classe e a view para inserção de chaves do thinspeak

---
+++ app/src/main/java/com/st/smarttrash/InsertKey.java
@@ -0,0 +1,20 @@
+package com.st.smarttrash;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by android on 05/07/17.
+ */
+
+class InsertKey extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.insert_key_window);
+
+    }
+}
