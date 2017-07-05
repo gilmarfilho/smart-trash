89f1ffc10eb167370022788ce1c9f1b487ac9bc1
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2017-07-05 14:17:11 -0300

    Insertkey agora salva a chave em SharedPreferences

--- app/src/main/java/com/st/smarttrash/InsertKey.java
+++ app/src/main/java/com/st/smarttrash/InsertKey.java
@@ -1,11 +1,16 @@
 package com.st.smarttrash;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
- * Created by android on 05/07/17.
+ * Created by jorge teixeira on 05/07/17.
  */
 
 public class InsertKey extends Activity{
@@ -19,10 +24,49 @@ public class InsertKey extends Activity{
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
+        //Pega o tamanho da tela
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width*.8), (int) (height*.6));
+        //Define o tamanho da tela
+        getWindow().setLayout((int) (width*.8), (int) (height*.7));
+
+        Button btn = (Button) findViewById(R.id.button_inserir);
+
+        final TextView key = (TextView) findViewById(R.id.key);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                //Armazena a chave do thingspeak
+                armazenar(key.getText().toString());
+
+            }
+        });
+
+    }
+
+    /**
+     * Armazena um valor a SharedPreferences
+     * @param key valor a ser armazenado
+     */
+    private void armazenar(String key){
+
+        //Verifica se a chave é do tamanho correto
+        if(key.length() == 16) {
+
+            // Precisamos de um objeto Editor para fazer mudanças de preferência.
+            // Todos os objetos são de android.context.Context
+            SharedPreferences settings = getSharedPreferences("chave", MODE_PRIVATE);
+            SharedPreferences.Editor editor = settings.edit();
+
+            editor.putString(key.toUpperCase(), "chave");
+
+            // Commit as edições
+            editor.commit();
+        }
 
     }
 }
