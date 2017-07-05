af02663b9fb671f90e68d6b7ce929fa09890afb8
Vinicius Carvalho Lopes <vinilopes03@gmail.com>
2017-07-05 15:34:39 -0300

    Many changes

--- app/src/main/java/com/st/smarttrash/MainActivity.java
+++ app/src/main/java/com/st/smarttrash/MainActivity.java
@@ -1,10 +1,15 @@
 package com.st.smarttrash;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -37,5 +42,6 @@ public class MainActivity extends AppCompatActivity {
                 frag.updateStatus();
             }
         });
+
     }
 }
