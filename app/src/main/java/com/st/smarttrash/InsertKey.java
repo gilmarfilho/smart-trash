604c4507e1e0f856c0f720be6214ad2b28ed9d0a
Jorge Teixeira <jorgetbruno@hotmail.co.uk>
2017-07-05 10:24:39 -0300

    Tela para inserir api do thingspeak

--- app/src/main/java/com/st/smarttrash/InsertKey.java
+++ app/src/main/java/com/st/smarttrash/InsertKey.java
@@ -2,19 +2,27 @@ package com.st.smarttrash;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
 
 /**
  * Created by android on 05/07/17.
  */
 
-class InsertKey extends Activity{
+public class InsertKey extends Activity{
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.insert_key_window);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*.8), (int) (height*.6));
 
     }
 }
